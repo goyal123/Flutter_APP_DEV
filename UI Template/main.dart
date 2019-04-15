ba99eee8ce4b90f2c5ff4a2697493133080f6f8c
Akhil Goyal <akhilg642@gmail.com>
2019-04-15 18:51:08 +0530

    Add files via upload

---
+++ UI Template/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterdrawer/new_page.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget 
+{
+  @override
+  Widget build(BuildContext context) 
+  {
+    return new  MaterialApp(
+      theme: new ThemeData(primarySwatch: Colors.deepPurple),
+      home: new HomePage(),
+      );
+  }
+}
+
+class HomePage  extends StatelessWidget 
+{
+  @override
+  Widget build(BuildContext context)
+   {
+    return new Scaffold
+    (
+      appBar: new AppBar
+      (
+        title: new Text("Drawer App"),
+        elevation: defaultTargetPlatform == TargetPlatform.android ? 5.5 : 0.0,
+      ),
+      drawer: new Drawer
+      (
+        child: new ListView
+                           (
+                             children: <Widget>[
+                                                 new UserAccountsDrawerHeader(
+                                                                                accountName: new Text("Akhil Goyal"),
+                                                                                accountEmail: new Text("akhilg642l@gmail.com"),
+                                                                                currentAccountPicture: new CircleAvatar(
+                                                                                                                         backgroundColor: Colors.white,
+                                                                                                                         child: new Text("Ak"),
+                                                                                                                       ),
+                                                                                otherAccountsPictures: <Widget>[
+                                                                                                                new CircleAvatar(
+                                                                                                                                  backgroundColor: Colors.white,
+                                                                                                                                  child: new Text("k"),
+                                                                                                                                )
+                                                                                                                ],
+                                                                              ),
+                                                  
+                                                  new ListTile(
+                                                                title: new Text("Page one"),
+                                                                trailing: new Icon(Icons.arrow_upward),
+                                                                onTap: ()
+                                                                { Navigator.of(context).pop();
+                                                                  Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context)=>new NewPage("Page One")));
+
+                                                                },
+                                                              ),
+            
+                                                  new ListTile(
+                                                                title: new Text("Page Two"),
+                                                                trailing: new Icon(Icons.arrow_downward),
+                                                                onTap: ()
+                                                                { Navigator.of(context).pop();
+                                                                  Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context)=>new NewPage("Page Two")));
+
+                                                                },
+                                                              ),
+                                                  new Divider(),
+                                                  new ListTile(
+                                                    title: new Text("Close"),
+                                                    trailing: new Icon(Icons.close),
+                                                    onTap: ()=> Navigator.of(context).pop(),
+                                                    
+                                                  ),
+                                        
+                                                 ],
+                            ),
+      ),
+
+      body: new Container(
+                            child: new Center(
+                                                child: new Text("Home Page")
+                                             ),
+                         )
+    );
+  }
+}
